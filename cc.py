6ce2cc368d4517cf3f258d2b7386f2d491dd9984
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-12-31 15:02:15 +0530

    Create cc.py

---
+++ cc.py
@@ -0,0 +1,18 @@
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+from telegram.ext import InlineQueryHandler
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
