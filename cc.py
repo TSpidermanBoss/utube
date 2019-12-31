43e32317d3588b93de97ccf356872923c29a4967
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-12-31 15:06:36 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -1,18 +1,9 @@
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
+from telegram.ext import Updater
+updater = Updater(token='TOKEN', use_context=True)
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-from telegram.ext import InlineQueryHandler
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
+updater.start_polling()
